a7ecdff2d41f075e813c03c78c6188a186edc0dc
Juan Carrillo <u631228@T0003540168.fibertel.com.ar>
2021-09-13 20:34:47 -0300

    add socketContext and useSocket

---
+++ citybike-client/src/hooks/useSocket.js
@@ -0,0 +1,13 @@
+import { useMemo } from "react";
+import io from "socket.io-client";
+
+export const useSocket = (serverPath) => {
+  const socket = useMemo(
+    () => io.connect(serverPath, { transports: ["websocket"] }),
+    [serverPath]
+  );
+
+  return {
+    socket,
+  };
+};
