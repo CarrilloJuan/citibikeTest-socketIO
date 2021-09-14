fac2094cba947771adaa26e145a57e3f51a99d7e
Juan Carrillo <u631228@T0003540168.fibertel.com.ar>
2021-09-13 22:04:46 -0300

    display bikes makers on the map

--- citybike-client/src/components/Map.js
+++ citybike-client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import Marker from "./Marker";
 import { SocketContext } from "../context/socketContext";
@@ -7,15 +7,31 @@ const position = [25.761681, -80.191788];
 const zoom = 13;
 
 export default function Map() {
-  const [availableBikes] = useState([]);
+  const [availableBikes, setAvailableBikes] = useState([]);
   const { socket } = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on("available-bikes", (bikes) => {
+      const bikeStations = bikes.stations;
+      if (bikeStations) {
+        setAvailableBikes(bikeStations);
+      }
+    });
+  }, [socket]);
+
   return (
     <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[25.72805, -80.24173]} />
+      {availableBikes.map((bike) => (
+        <Marker
+          key={bike.id}
+          position={[bike.latitude, bike.longitude]}
+          {...bike}
+        />
+      ))}
     </MapContainer>
   );
 }
