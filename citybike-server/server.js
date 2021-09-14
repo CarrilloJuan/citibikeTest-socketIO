8d1a4bcd48d6e2c9a198d155446b3569118e6ab8
Juan Carrillo <u631228@T0003540168.fibertel.com.ar>
2021-09-13 21:29:38 -0300

    updated socket-io server

--- citybike-server/server.js
+++ citybike-server/server.js
@@ -1,33 +1,36 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
-
-
+var cors = require("cors");
+const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach";
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
 
+app.use(cors());
 
 app.use(index);
 
 const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+}); // < Interesting!
 let interval;
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
-  console.log('New connection ' + socketId + ' from ' + clientIp);
+  console.log("New connection " + socketId + " from " + clientIp);
+
+  io.emit("available-bikes", []);
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
 
-
-
 server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
